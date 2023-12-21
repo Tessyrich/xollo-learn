c668993e9bf5b1cf270ece0482af53497b2d1d79
Tessyrich <olyrich3@gmail.com>
2023-12-21 15:55:01 +0100

    Add new interactions

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+const cards = document.querySelectorAll(".group");
 
+cards.forEach((card, index) => {
+  // Check if the current card is not the last one
+
+  if (index !== cards.length - 1) {
+    card.addEventListener("mouseover", () => {
+      const nextCard = cards[(index + 1) % cards.length];
+      const overlay = nextCard.querySelector(".overlay");
+      overlay.classList.remove("hidden");
+      console.log(overlay);
+    });
+
+    card.addEventListener("mouseout", () => {
+      const nextCard = cards[(index + 1) % cards.length];
+      const overlay = nextCard.querySelector(".overlay");
+      overlay.classList.add("hidden");
+    });
+  }
+  if (index === cards.length - 1) {
+    card.addEventListener("mouseover", () => {
+      const prevCard = cards[(index - 1 + cards.length) % cards.length];
+      const overlay = prevCard.querySelector(".overlay");
+      overlay.classList.remove("hidden");
+    });
+    card.addEventListener("mouseout", () => {
+      const prevCard = cards[(index - 1 + cards.length) % cards.length];
+      const overlay = prevCard.querySelector(".overlay");
+      overlay.classList.add("hidden");
+
+      const firstCard = cards[0];
+      const firstCardOverlay = firstCard.querySelector(".overlay");
+      firstCardOverlay.classList.add("hidden");
+    });
+  }
+});
---
+++ test.js
@@ -0,0 +1,9 @@
+// const selectAll = (element) => document.querySelectorAll(element);
+// const selectCard = selectAll(".overlay");
+// const selectGroup = selectAll(".group2");
+
+// selectGroup.forEach((element, index) => {
+//   element.addEventListener("mouseover", () => {
+//     console.log(selectCard[index] );
+//   });
+// });
